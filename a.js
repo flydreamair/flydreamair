fb2d1ac50cee6831a37db7ce30f96f3bc25be3bb
lj428 <69785383+lj428@users.noreply.github.com>
2020-10-18 19:39:32 +1100

    Add files via upload

--- a.js
+++ a.js
@@ -1,75 +1,91 @@
-var users = [];
+
+$(function(){
+
+	var users = [];
+
+	if (JSON.parse(localStorage.getItem("users")) != null)
+		users = JSON.parse(localStorage.getItem("users"));
+
+	$("#login").on('click', function(){
+	
+		let uname = $("#uname_login").val().trim();
+		let pword = $("#pword_login").val().trim();
+
+		let user_check = false;
+
+		for (let i = 0; i < users.length; i++)
+			if (uname == users[i].username && pword == users[i].password)
+				user_check =  true;
 
 
-if (JSON.parse(localStorage.getItem("users")) != null)
-{
- users = JSON.parse(localStorage.getItem("users"));
-}
+		if (user_check){
 
-function register_user()
-{
- let uname = document.getElementById("uname").value.trim();
- let pword = document.getElementById("pword").value.trim();
+			$("#out").text("Login succeded!");
+			$("#uname_login").val("");
+			$("#pword_login").val("");
+			sessionStorage.setItem("active_user", uname);
+			location.replace("indexb.html");
 
- if (!user_check_r(uname))
-{
-   
-     users.push({username:uname, password:pword});
-	 document.getElementById("uname").value = "";
-	 document.getElementById("pword").value = "";
-	 document.getElementById("register_out").innerHTML = "Register successful";
-	localStorage.setItem("users", JSON.stringify(users));
-
-
-   }else if (user_check(uname, pword)){
-   
-		document.getElementById("register_out").innerHTML = "User already exists!";
-
-   }
-}
-
-function user_check_r(uname){
-
-	for (let i = 0; i < users.length; i++){
-
-		if (uname == users[i].username)
-			return true;
-
-	}
-
-	return false
-
-}
-
-function user_check(uname, pword){
-
-	for (let i = 0; i < users.length; i++){
-
-		if (uname == users[i].username && pword == users[i].password)
-			return true;
-
-	}
-
-	return false;
-
-}
-
-function login(){
-
-	let uname = document.getElementById("uname_login").value.trim();
-	let pword = document.getElementById("pword_login").value.trim();
-
-	if (user_check(uname, pword)){
-
-		document.getElementById("out").innerHTML = "Login succeded!";
-		document.getElementById("uname_login").value = "";
-		document.getElementById("pword_login").value = "";
-		location.replace("indexb.html");
-
-	} else {
+		} else
+			$("#out").text("Login failed :(");
 		
-		document.getElementById("out").innerHTML = "Login failed :(";
+	});
 
-	}
+	$("#register_user").on('click', function(){
 
-}
+		let uname = $("#uname").val().trim();
+		let pword = $("#pword").val().trim();
+
+		let user_check_r = false;
+
+		for (let i = 0; i < users.length; i++)
+			if (uname == users[i].username)
+				user_check_r =  true;
+
+		if (!user_check_r){
+	
+		users.push({
+
+			username:uname, 
+			password:pword
+
+		});
+
+		$("#uname").val("");
+		$("#pword").val("");
+		$("#register_out").text("Register successful");
+		localStorage.setItem("users", JSON.stringify(users));
+
+		} else
+			$("#register_out").text("User already exists!");
+		
+	});
+
+	$("#state-select").change(function() {
+
+		var state = $(this).val();
+
+		if (state == "") {
+			$("#state_covid_info").html(noselect_covid_html);
+		} else if (state == "ACT") {
+			$("#state_covid_info").html(ACT_covid_html);
+		} else if (state == "NSW") {
+			$("#state_covid_info").html(NSW_covid_html);
+		} else if (state == "NT") {
+			$("#state_covid_info").html(NT_covid_html);
+		} else if (state == "QLD") {
+			$("#state_covid_info").html(QLD_covid_html);
+		} else if (state == "TAS") {
+			$("#state_covid_info").html(TAS_covid_html);
+		} else if (state == "VIC") {
+			$("#state_covid_info").html(VIC_covid_html);
+		} else if (state == "WA") {
+			$("#state_covid_info").html(WA_covid_html);
+		} else {
+			$("#state_covid_info").html("Selection Error");
+		}
+
+
+	});
+
+});
